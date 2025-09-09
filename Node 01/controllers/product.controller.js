8e30a938459bc65f6caaa9b8797d762bdfd8450d
Bhargav Jagani <jaganibhargav30@gmail.com>
2025-09-09 09:51:17 +0530

    product api

--- Node 01/controllers/product.controller.js
+++ Node 01/controllers/product.controller.js
@@ -11,3 +11,42 @@ export const postProduct = async (req, res) => {
         return res.status(500).json({message : "Server error"})
     }
 }
+
+export const getProduct = async (req, res) => {
+    try {
+        const products = await Product.find()
+        return res.status(200).json({message : "Product get Successfully", products})
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({message : "Server error"})
+    }
+}
+
+export const deleteProduct = async (req, res) => {
+    try {
+        const {id} = req.params;
+        await Product.findByIdAndDelete(id)
+        return res.status(200).json({message : "Product deleted"})
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({message : "Server error"})
+    }
+}
+
+
+export const updateProduct = async (req, res) => {
+    try {
+        const {id} = req.params
+        const {title, description, price} = req.body
+        const image = req?.file?.filename
+        const formData = {title, description, price}
+        if(image){
+            formData.image = image
+        }
+        await Product.findByIdAndUpdate(id, formData)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({message : "Server error"})
+    }
+}
+
--- Node 01/routes/product.route.js
+++ Node 01/routes/product.route.js
@@ -1,8 +1,14 @@
 import express from 'express'
-import { postProduct } from "../controllers/product.controller.js";
+import { postProduct, getProduct, deleteProduct, updateProduct } from "../controllers/product.controller.js";
 import upload from '../utils/multer.js';
 const router = express.Router()
 
-router.post('/postproduct', upload.single('image') ,postProduct)
+router.post('/postproduct', upload.single('image') ,postProduct);
+
+router.get('/getproduct', getProduct)
+
+router.delete('/deleteproduct/:id', deleteProduct)
+
+router.put('/updateproduct/:id', upload.single('image') ,updateProduct)
 
 export default router;
