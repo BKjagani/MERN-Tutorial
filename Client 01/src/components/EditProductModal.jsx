48db7dab59b23d459746f712a69fb2fa31034b4e
Bhargav Jagani <jaganibhargav30@gmail.com>
2025-09-17 09:47:53 +0530

    update product

---
+++ Client 01/src/components/EditProductModal.jsx
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+function EditProductModal({editObj}) {
+    console.log(editObj)
+  const POST_PRODUCT_API = "http://localhost:3000/api/updateproduct";
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    price: "",
+  });
+  const [image, setImage] = useState(null);
+
+  function handleChange(e) {
+    setFormData((preval) => {
+      return { ...preval, [e.target.name]: e.target.value };
+    });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+    } catch (error) {}
+  }
+
+  return (
+    <div>
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
+                Modal title
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form action="" onSubmit={handleSubmit}>
+                <div className="input-group mb-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Title"
+                    aria-label="Title"
+                    aria-describedby="basic-addon1"
+                    name="title"
+                    value={formData.title}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="input-group mb-3">
+                  <textarea
+                    placeholder="Description"
+                    className="form-control"
+                    aria-label="With textarea"
+                    name="description"
+                    value={formData.description}
+                    onChange={handleChange}
+                  ></textarea>
+                </div>
+                <div className="input-group mb-3">
+                  <input
+                    type="number"
+                    className="form-control"
+                    placeholder="Price"
+                    aria-label="Price"
+                    aria-describedby="basic-addon1"
+                    name="price"
+                    value={formData.price}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="input-group mb-3">
+                  <input
+                    type="file"
+                    className="form-control"
+                    onChange={(e) => {
+                      setImage(e.target.files[0]);
+                    }}
+                    required
+                  />
+                </div>
+                <button type="submit" className="btn btn-success">
+                  Submit
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-secondary ms-3"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+              </form>
+            </div>
+            <div className="modal-footer"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default EditProductModal;
--- Client 01/src/components/ProductTable.jsx
+++ Client 01/src/components/ProductTable.jsx
@@ -1,63 +1,94 @@
 import React, { useEffect, useState } from "react";
 import { fetchData, deleteProduct } from "../utils/api";
+import EditProductModal from "./EditProductModal";
 
 function ProductTable() {
   const IMAGE_URL = "http://127.0.0.1:3000/uploads";
   const [productList, setProductList] = useState([]);
-  const [refresh, setRefresh] = useState(false)
+  const [refresh, setRefresh] = useState(false);
+  const [editObj, setEditObj] = useState({})
 
   useEffect(() => {
     const main = async () => {
-        const response = await fetchData();
-        setProductList(response)
-    }
-    main()
-    setRefresh(false)
+      const response = await fetchData();
+      setProductList(response);
+    };
+    main();
+    setRefresh(false);
   }, [refresh]);
 
-  async function handleDelete(id){
-    const response = await deleteProduct(id)
-     setRefresh(true)
+  async function handleDelete(id) {
+    const response = await deleteProduct(id);
+    setRefresh(true);
   }
 
-  return <div>
+  function handleUpdate(id){
+   const obj = productList.find((ele) => ele._id === id)
+   setEditObj(obj)
+  }
+
+  return (
+    <div>
       <div className="container-fluid">
         <div className="row">
           <div className="col-12">
-             <table className="table">
-                <thead>
-                    <tr>
-                       <th>Sr no.</th>
-                       <th>Image</th>
-                       <th>Title</th>
-                       <th>Description</th>
-                       <th>Price</th>
-                       <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                   {productList.length > 0 && productList.map((product, index) => {
-                     return <tr key={product._id}>
-                         <td>{index + 1}</td>
-                         <td>
-                            <img src={`${IMAGE_URL}/${product.image}`} alt="" style={{height : "70px"}}/>
-                         </td>
-                         <td>{product.title}</td>
-                         <td>{product.description}</td>
-                         <td>{product.price}</td>
-                         <td>
-                            <button className="btn text-danger fs-3" onClick={() => {handleDelete(product._id)}}>
-                                <i className="bi bi-trash3-fill"></i>
-                            </button>
-                         </td>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Sr no.</th>
+                  <th>Image</th>
+                  <th>Title</th>
+                  <th>Description</th>
+                  <th>Price</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {productList.length > 0 &&
+                  productList.map((product, index) => {
+                    return (
+                      <tr key={product._id}>
+                        <td>{index + 1}</td>
+                        <td>
+                          <img
+                            src={`${IMAGE_URL}/${product.image}`}
+                            alt=""
+                            style={{ height: "70px" }}
+                          />
+                        </td>
+                        <td>{product.title}</td>
+                        <td>{product.description}</td>
+                        <td>{product.price}</td>
+                        <td>
+                          <button
+                            className="btn text-danger fs-3"
+                            onClick={() => {
+                              handleDelete(product._id);
+                            }}
+                          >
+                            <i className="bi bi-trash3-fill"></i>
+                          </button>
+                          <button
+                            onClick={() => {handleUpdate(product._id)}}
+                            className="btn text-primary fs-3"
+                            type="button"
+                            data-bs-toggle="modal"
+                            data-bs-target="#exampleModal"
+                          >
+                            <i className="bi bi-pencil-square"></i>
+                          </button>
+                        </td>
                       </tr>
-                   }) }
-                </tbody>
-             </table>
+                    );
+                  })}
+              </tbody>
+            </table>
+            <EditProductModal  editObj={editObj}/>
           </div>
         </div>
       </div>
-  </div>;
+    </div>
+  );
 }
 
 export default ProductTable;
